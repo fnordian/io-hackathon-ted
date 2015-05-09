b7137910b3b34020bdb1cc3289b646625bd59f2d
Corinna Baldauf <baldauf@workstation-247.netzquadrat.de>
2015-05-09 12:42:48 +0200

    connected controller and view; reading var from controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,8 +8,11 @@ define(function() {
 
 var controllers = {};
 
-controllers.MyCtrl1 = function() {}
-controllers.MyCtrl1.$inject = [];
+controllers.resultsCtrl = function($scope, $resource) {
+    //functionality here!
+    $scope.resultA = 14;
+}
+controllers.resultsCtrl.$inject = ['$scope', '$resource'];
 
 controllers.MyCtrl2 = function() {}
 controllers.MyCtrl2.$inject = [];
--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,8 @@
 requirejs.config({
   paths: {
     'angular': ['../lib/angularjs/angular'],
-    'angular-route': ['../lib/angularjs/angular-route']
+    'angular-route': ['../lib/angularjs/angular-route'],
+    'angular-resource': ['../lib/angularjs/angular-resource']
   },
   shim: {
     'angular': {
@@ -14,21 +15,22 @@ requirejs.config({
     'angular-route': {
       deps: ['angular'],
       exports : 'angular'
+    },
+    'angular-resource': {
+      deps: ['angular'],
+      exports : 'angular'
     }
   }
 });
 
-require(['angular', './controllers', './directives', './filters', './services', 'angular-route'],
+require(['angular', './controllers', './directives', './filters', './services', 'angular-route', 'angular-resource'],
   function(angular, controllers) {
 
     // Declare app level module which depends on filters, and services
 
-    angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
-      config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
-        $routeProvider.otherwise({redirectTo: '/view1'});
-      }]);
+    angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).controller('resultsCtrl', ['$scope', function($scope) {
+                        $scope.resultA = 14;
+            }]);
 
     angular.bootstrap(document, ['myApp']);
 
