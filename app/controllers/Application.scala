67bac80c1e53305f10814531ce1aab62c8897b47
Corinna Baldauf <baldauf@workstation-247.netzquadrat.de>
2015-05-09 14:44:38 +0200

    begin evaluating new call submit. not working, yet

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 import play.api.http.MimeTypes
+import 
 
 object Application extends Controller {
 
@@ -13,7 +14,23 @@ object Application extends Controller {
     Ok("{ \"ted\" : [ [ 23, 5 ] ] } ")
   }
 
+  
+  case class NewCall(to: String)
+  
+  val newCallForm = Form(
+      mapping(
+          "to" -> text)
+      )(NewCall.apply)(NewCall.unapply)
+
+  var counter = Map("4915792341043"-> 0, "4915792335891"-> 0)
   def newCall() = Action {
+      
+      newCallForm.bindFromRequest.fold(
+          formData => {
+              val newCallData = NewCall(formData.to)
+              print(newCallData.to)
+          }
+          )
     Ok("")
   }
 }
